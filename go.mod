03cb42b57e915da14d46f692a902ce8cdddb4c9e
stumburs <stumburs@inbox.lv>
2024-03-07 18:25:07 +0200

    Created window

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/stumburs/floppy_gopher
 
 go 1.22.1
+
+require github.com/gen2brain/raylib-go/raylib v0.0.0-20240227114648-c3665eb9abf8
+
+require (
+	github.com/ebitengine/purego v0.6.0-alpha.1.0.20231122024802-192c5e846faa // indirect
+	golang.org/x/sys v0.14.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
 
 func main() {
-	fmt.Println("Hello!")
+
+	rl.InitWindow(640, 960, "floppy gopher")
+	defer rl.CloseWindow()
+
+	rl.SetTargetFPS(60)
+
+	for !rl.WindowShouldClose() {
+		// Update
+
+		// Draw
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.Black)
+		rl.EndDrawing()
+	}
 }
