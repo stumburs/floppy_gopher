1d19eaeaafb9450fb861de052c07803ca349e12b
stumburs <stumburs@inbox.lv>
2024-03-07 19:19:09 +0200

    Tweaked velocity

--- gopher/gopher.go
+++ gopher/gopher.go
@@ -21,7 +21,7 @@ func (gopher *Gopher) Update(dt float32) {
 	gopher.vel += gopher.gravity * dt
 
 	if rl.IsKeyPressed(rl.KeySpace) {
-		gopher.vel = -gopher.gravity * 0.5
+		gopher.vel = -gopher.gravity * 0.3
 	}
 
 	gopher.pos.Y += gopher.vel * dt
@@ -32,7 +32,7 @@ func NewGopher(sizeX, sizeY float32) *Gopher {
 		pos:     rl.Vector2{X: float32(rl.GetScreenWidth())/4 - sizeX/2, Y: float32(rl.GetScreenHeight())/2 - sizeY/2},
 		size:    rl.Vector2{X: sizeX, Y: sizeY},
 		vel:     0,
-		gravity: 1000,
+		gravity: 1500,
 	}
 	return &gopher
 }
